b5d58099e4b4725a205df15f1aebb9f690c993b5
imsushant12 <sushantgaurav57@gmail.com>
2024-08-05 01:04:07 +0530

    Added Color Changer Code

---
+++ Color_Changer/script.js
@@ -0,0 +1,19 @@
+function colorChangeGrey() {
+  document.body.style.backgroundColor = "grey";
+  document.body.style.color = "black";
+}
+
+function colorChangeWhite() {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+}
+
+function colorChangeBlue() {
+  document.body.style.backgroundColor = "blue";
+  document.body.style.color = "white";
+}
+
+function colorChangeYellow() {
+  document.body.style.backgroundColor = "yellow";
+  document.body.style.color = "black";
+}
