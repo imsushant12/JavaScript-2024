422ae4af6b79746eee344e15361a4fde7780a5bf
imsushant12 <sushantgaurav57@gmail.com>
2024-08-15 00:00:56 +0530

    added code for day-1s

---
+++ 30_Days_JS_Challenge/Day-1/Day_1_Solution.js
@@ -0,0 +1,41 @@
+// Task-1:
+var number = 10;
+console.log(number);
+
+// Task-2:
+let myName = "Sushant";
+console.log(myName);
+
+// Task-3:
+const flag = true;
+console.log(flag);
+
+// Task-4:
+let num = 25;
+let fullName = "Sushant Gaurav";
+let isValid = false;
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let animal = {
+  name: "Dog",
+  age: 12,
+  breed: "Husky",
+  color: "white",
+};
+
+console.log(num);
+console.log(fullName);
+console.log(isValid);
+console.log(numbers);
+console.log(animal);
+// Can also use console.table();
+
+// Task-5:
+let age = 18;
+console.log(age);
+age = 25;
+console.log(age);
+
+// Task-6
+flag = false;
+console.log(flag);
+// ERROR- TypeError: Assignment to constant variable.
