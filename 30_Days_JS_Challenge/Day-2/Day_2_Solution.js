eb23aec4f3fa7482d6cc3f95e9cb3b9babdc88eb
imsushant12 <sushantgaurav57@gmail.com>
2024-08-15 23:36:47 +0530

    added code for day-2

---
+++ 30_Days_JS_Challenge/Day-2/Day_2_Solution.js
@@ -0,0 +1,48 @@
+// Task-1:
+let num1 = 20;
+let num2 = 10;
+console.log(num1 + num2);
+
+// Task-2:
+console.log(num1 - num2);
+
+// Task-3:
+console.log(num1 * num2);
+
+// Task-4:
+console.log(num1 / num2);
+
+// Task-5:
+console.log(num1 % num2);
+
+// Task-6:
+num1 += 20;
+console.log(num1);
+
+// Task-7:
+num1 -= 20;
+console.log(num1);
+
+// Task-8, 9, 10:
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+// Task-11, 12:
+console.log(num1 > num2 && num1 > 0);
+console.log(num1 < num2 || num1 > 0);
+
+// Task-13:
+let notResult = num1 > num2;
+console.log(!notResult);
+
+// Task-14:
+let ternaryResult =
+  num1 > num2
+    ? "First Number is larger than second"
+    : "Second number is larger than first";
+
+console.log(ternaryResult);
